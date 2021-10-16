eab105fed3f4c57d56fabf39629bc3f367150d69
Michael Kennedy <mikeckennedy@gmail.com>
2021-10-16 12:31:39 -0700

    Now testing the main cases, with error conditions.

--- tests/test_rendering.py
+++ tests/test_rendering.py
@@ -32,3 +32,20 @@ def test_render_with_data(registered_extension):
     age = 32
     html: chameleon_partials.HTML = chameleon_partials.render_partial('render/with_data.pt', name=name, age=age)
     assert f'<span>Your name is {name} and age is {age}</span>' in html.html_text
+
+
+def test_render_with_layout(registered_extension):
+    value_text = "The message is clear"
+    html: chameleon_partials.HTML = chameleon_partials.render_partial('render/with_layout.pt', message=value_text)
+    assert '<title>Chameleon Partials Test Template</title>' in html.html_text
+    assert value_text in html.html_text
+
+
+def test_missing_template(registered_extension):
+    with pytest.raises(ValueError):
+        chameleon_partials.render_partial('no-way.pt', message=7)
+
+
+def test_not_registered():
+    with pytest.raises(chameleon_partials.PartialsException):
+        chameleon_partials.render_partial('doesnt-matter.pt', message=7)
